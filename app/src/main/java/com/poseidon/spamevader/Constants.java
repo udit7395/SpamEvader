75cc9dbb3e8d450a6b7d42655b658b025423bd88
udit7395 <udit7395@gmail.com>
2019-01-11 13:37:44 +0530

    Update Flaticon Credit URL String

--- app/src/main/java/com/poseidon/spamevader/Constants.java
+++ app/src/main/java/com/poseidon/spamevader/Constants.java
@@ -10,5 +10,5 @@ public class Constants {
     public static final String DEVELOPER_DETAILS = "Made By\n Udit Patadia";
     public static final String GITHUB_DEVELOPER_URL = "https://github.com/udit7395";
 
-    public static final String ICON_CREDIT_URL = "Icons made by <a href=\"http://www.freepik.com\" title=\"Freepik\">Freepik</a> from <a href=\"https://www.flaticon.com/\" title=\"Flaticon\">www.flaticon.com</a> is licensed by <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a>";
+    public static final String ICON_CREDIT_URL = "App Icon made by <a href=\"http://www.freepik.com\" title=\"Freepik\">Freepik</a> from <a href=\"https://www.flaticon.com/\" title=\"Flaticon\">www.flaticon.com</a> is licensed by <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a>";
 }
